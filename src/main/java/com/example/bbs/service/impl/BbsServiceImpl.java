295da878930aee7f65fd50e364c129ba79175990
정진룡 <jinlyong1020@gmail.com>
2023-06-14 09:59:09 +0900

    1. lombok 2. basePath demo -> bbs로 수정

--- src/main/java/com/example/bbs/service/impl/BbsServiceImpl.java
+++ src/main/java/com/example/bbs/service/impl/BbsServiceImpl.java
@@ -9,15 +9,15 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.bbs.dao.BbsMapper;
 import com.example.bbs.service.BbsService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
 @Service
 public class BbsServiceImpl implements BbsService{
 
 	private final BbsMapper bbsMapper;
-	
-	public BbsServiceImpl(BbsMapper bbsMapper) {
-		this.bbsMapper = bbsMapper;
-	}
-	
 	
 	@Transactional(readOnly = true)
 	@Override
--- src/main/java/com/example/bbs/web/BbsController.java
+++ src/main/java/com/example/bbs/web/BbsController.java
@@ -3,23 +3,30 @@ package com.example.bbs.web;
 import java.util.HashMap;
 import java.util.List;
 
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bbs.service.BbsService;
+import com.example.bbs.service.impl.BbsServiceImpl;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Slf4j
+@Tag(name = "게시판 API", description = "게시판 API")
 @RestController
 @RequestMapping("/api/bbs")
 public class BbsController {
 
 	private final BbsService bbsService;
 	
-	public BbsController(BbsService bbsService) {
-		this.bbsService = bbsService;
-	}
-
+	@Operation(summary = "게시판 내역 조회", description = "게시판 내역 조회")
 	@GetMapping("/list")
 	public ResponseEntity<List<HashMap<String, String>>> listBbs() {
 		
